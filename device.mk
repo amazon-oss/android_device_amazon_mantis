6c8178382c0b86ca94cafdd92c274318850b0f96
R0rt1z2 <me@r0rt1z2.com>
2024-07-07 17:44:24 +0200

    mantis: Import minimal rootdir
    Change-Id: I1c04eb1bfd55ef0e9288063e658008e6b6d9a02f

--- device.mk
+++ device.mk
@@ -7,6 +7,19 @@
 # Characteristics
 PRODUCT_CHARACTERISTICS := nosdcard,tv
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    fstab.mt8695 \
+    init.mt8695.rc \
+    init.mt8695.usb.rc \
+    ueventd.mt8695.rc
+
+ifneq ($(filter eng userdebug,$(TARGET_BUILD_VARIANT)),)
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.adb.secure=0 \
+    ro.secure=0
+endif
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/amazon/mantis/mantis-vendor.mk)
 

63d41f86b9ec6da60ede8f7848f65b3c88b2cb70
R0rt1z2 <me@r0rt1z2.com>
2024-07-07 17:01:38 +0200

    mantis: Set Architecture flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,13 @@
 
 DEVICE_PATH := device/amazon/mantis
 
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_ARCH_VARIANT_CPU := cortex-a53
+
 # Inherit the proprietary files
 include vendor/amazon/mantis/BoardConfigVendor.mk

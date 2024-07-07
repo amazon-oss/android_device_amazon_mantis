a48b009b6a82a302a1757d9fb8def2b3af7932ee
R0rt1z2 <me@r0rt1z2.com>
2024-07-07 19:17:27 +0200

    mantis: Configure Bluetooth stack

---
+++ bluetooth/bdroid_buildcfg.h
@@ -0,0 +1,13 @@
+/*
+* Copyright (C) 2024 The LineageOS Project
+*
+* SPDX-License-Identifier: Apache-2.0
+*/
+
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+
+#define BTM_DEF_LOCAL_NAME "Fire TV"
+
+#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
+#endif
